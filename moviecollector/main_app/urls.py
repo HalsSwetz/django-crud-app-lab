4ff134eea02b5690cec628681c4af2d4f523328b
Halsey Svetz <halseycheese@gmail.com>
2025-04-01 15:57:19 -0600

    add the details page, model, and some css

--- moviecollector/main_app/urls.py
+++ moviecollector/main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('movies/', views.movie_index, name='movie-index'),
+    path('movies/<int:movie_id>/', views.movie_detail, name='movie-detail')
 ]
 
 
